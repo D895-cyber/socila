6afd5d0d55514d4888fe4073f1e962bdd646472f
DEV <CHIRAGGULATI416@GMAIL.COM>
2025-08-31 17:44:31 +0530

    ddd

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require("path")
+
+console.log(path.resolve(__dirname,"../frontend/build"));
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,"../frontend/build")))
+const PORT = process.env.PORT || 5000;
+async function connectDB(){
+    try{
+        const res = await mongoose.connect('mongodb+srv://preetam:preetam@cluster0.wg3g3.mongodb.net/socialmedia?retryWrites=true&w=majority&appName=Cluster0')
+        console.log('Connected to MongoDB:', res.connection.host);
+    }
+    catch(err){
+        console.log('Error connecting to MongoDB:', err.message);
+        
+
+    }
+}
+
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
+app.use(require('./routes/user'));
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"../frontend/build","index.html"))
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
